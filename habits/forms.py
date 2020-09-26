3468a2ec4cf27a556088067ae2d5188150ca0ba6
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-09-26 17:54:30 -0400

    setting up basics

---
+++ habits/forms.py
@@ -0,0 +1,22 @@
+from django.forms import ModelForm, Form, CharField, ChoiceField
+from .models import Habit
+
+
+class HabitForm(ModelForm):
+    class Meta:
+        model = Habit
+        fields = [
+            'title',
+            'body',
+        ]
+
+
+# class HabitSearchForm(Form):
+#     SEARCH = [
+#         ("contains", "contains"),
+#         ("exact match", "exact match"),
+#     ]
+#     title = CharField()
+#     title_search_by = ChoiceField(choices=SEARCH)
+#     body = CharField()
+#     body_search_by = ChoiceField(choices=SEARCH)
