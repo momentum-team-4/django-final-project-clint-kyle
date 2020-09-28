41980fbccafe85c9a2ce3cafc79a69fba9527df2
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-09-28 14:50:39 -0400

    working on users, need to install from pipfile

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.messages import success, error
+from .models import Habit
+from .forms import HabitForm
 
 # Create your views here.
+def habits_list(request):
+    habits = Habit.objects.all()
+    return render(request, "habits/habits_list.html", {"habits": habits})
